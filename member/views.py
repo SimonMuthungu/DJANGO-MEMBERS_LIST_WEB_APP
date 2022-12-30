49bb78357884e933069dc0d4c9d097cf2d3feaac
SimonMuthungu <henrymsechu1@gmail.com>
2022-12-30 04:32:57 +0300

    Initial commit

---
+++ member/views.py
@@ -0,0 +1,21 @@
+from django.template import loader
+from django.http import HttpResponse
+from .models import Member
+
+# Create your views here.
+def member(request):
+    mymembers = Member.objects.all().values()
+    template = loader.get_template('all_members.html') 
+    context = {
+        'mymembers': mymembers
+    }
+
+    return HttpResponse(template.render(context, request))
+
+def details(request, id):
+    mymembers = Member.objects.get(id=id)
+    template = loader.get_template('details.html')
+    context = {
+        'mymembers': mymembers
+    }
+    return HttpResponse(template.render(context, request))
